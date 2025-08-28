9e7e51407a76326c300c0c062bb3c5ad5d5433e7
Ambuj Kumar Tripathi <ambujonly761@gmail.com>
2025-08-29 00:44:09 +0530

    Fix PWA paths and push

--- sw.js
+++ sw.js
@@ -9,22 +9,3 @@ const urlsToCache = [
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
